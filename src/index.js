40242d8d657b39d6638ce381106b8d747849c255
Christian Hernandez <hernandezchristian94@gmail.com>
2023-02-16 08:40:13 -0800

    Co-authored-by: Gioeni <Gioeni@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    addToDo(e.target.newTaskDescription.value);
+  });
 });
+
+function addToDo(todo) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", deleteTask);
+
+  li.textContent = `${todo}`;
+  btn.textContent = "delete";
+
+  document.querySelector("ul").appendChild(li);
+  li.appendChild(btn);
+}
+
+function deleteTask(e) {
+  e.taget.parentElement.remove();
+}
